aece43c95c64b3d77cd2e02cbe44ff3ee208de9b
etienne1698 <etienne.robert1698@gmail.com>
2024-12-30 00:47:11 +0100

    update

--- bevy_test/src/plugins/basic_scene_plugin.rs
+++ bevy_test/src/plugins/basic_scene_plugin.rs
@@ -1,6 +1,24 @@
 use bevy::prelude::*;
+use rand::Rng;
 
 use bevy_rapier3d::prelude::*;
+
+fn generate_trees(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let tree_asset_3d = asset_server.load(GltfAssetLabel::Scene(0).from_asset("3d/tree.glb"));
+
+    let mut rng = rand::thread_rng();
+
+    for _ in 0..15 {
+        let x = rng.gen_range(-50.0..50.0); 
+        let z = rng.gen_range(-50.0..50.0);
+        let y = 0.0; 
+
+        commands.spawn((
+            SceneRoot(tree_asset_3d.clone()), 
+            Transform::from_xyz(x, y, z),
+        ));
+    }
+}
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     let player_asset_3d =
@@ -21,6 +39,8 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         },
         Transform::from_xyz(4.0, 8.0, 4.0),
     ));
+
+    generate_trees(commands, asset_server);
 }
 
 pub struct BasicScenePlugin;
--- bevy_test/src/systems/follow_camera.rs
+++ bevy_test/src/systems/follow_camera.rs
@@ -9,7 +9,7 @@ pub fn follow_camera<TToFollow: Component, TFollow: Component>(
     if let Ok(to_follow_transform) = to_follow_query.get_single() {
         for mut camera_transform in camera_query.iter_mut() {
             // Positionne la caméra à une distance relative par rapport au joueur
-            camera_transform.translation = to_follow_transform.translation + Vec3::new(0.0, 4.0, 8.0);
+            camera_transform.translation = to_follow_transform.translation + Vec3::new(0.0, 3.0, 8.0);
 
             // Oriente la caméra pour qu'elle regarde toujours vers le joueur
             camera_transform.look_at(to_follow_transform.translation, Vec3::Y);
